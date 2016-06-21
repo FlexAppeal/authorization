dc965d77593603defbfa96da7ce7312b3422e2ef
Guido Schmitz <guisch95@gmail.com>
2016-06-21 13:47:46 +0200

    Made the code pretty

--- test.js
+++ test.js
@@ -1,38 +1,27 @@
 const test = require('tape');
 const sinon = require('sinon');
-const authorization = require('./index');
-const checkRole = authorization.checkRole;
-const can = authorization.can;
-const check = authorization.check;
-const getUserRole = authorization.getUserRole;
-const getActionByKey = authorization.getActionByKey;
-
-const createExchangeRoles = ['employee', 'admin'];
-const actions = {
-  'create-exchange': createExchangeRoles,
-  'delete-exchange': {
-    role: 'admin',
-    validate: (user, exchange) => {
-      return exchange.user.id === user.id;
-    },
-  },
-};
+const { checkRole, can, check, getUserRole, getActionByKey } = require('./index');
 
 const employee = { id: 1, username: 'liam@flex-appeal.nl', role: 'employee' };
 const admin = { id: 2, username: 'guido@flex-appeal.nl', role: 'admin' };
-const teamleader = { id: 3, username: 'ruben@flex-appeal.nl', role: 'teamleader' };
-const exchangeA = { id: 1, title: 'Test shift', user: { id: 1 } };
-const exchangeB = { id: 2, title: 'Test shift', user: { id: 2 } };
 
-test('should return true when user user role is one of the required roles', t => {
-  t.true(checkRole('admin', 'admin'));
+test('should return true when user has one of the required roles', t => {
   t.true(checkRole('employee', ['admin', 'employee']));
   t.end();
 });
 
-test('should fail when user doesn\'t have the required role', t => {
-  t.false(checkRole('employee', 'admin'));
+test('should return true when user has one of the required role', t => {
+  t.true(checkRole('admin', 'admin'));
+  t.end();
+});
+
+test('should fail when user doesn\'t have the required role and value is array', t => {
   t.false(checkRole('employee', ['teamleader', 'admin']));
+  t.end();
+});
+
+test('should fail when user doesn\'t have the required role and value is string', t => {
+  t.false(checkRole('employee', 'admin'));
   t.end();
 });
 
@@ -43,8 +32,16 @@ test('should get the valid user role from the user object', t => {
 });
 
 test('should return the right action when getting an action by key', t => {
+  const createExchangeRoles = ['employee', 'admin'];
+
   authorization.setConfig({
-    actions,
+    actions: {
+      'create-exchange': createExchangeRoles,
+      'delete-exchange': {
+        role: 'admin',
+        validate: (user, exchange) => exchange.user.id === user.id,
+      },
+    },
   });
 
   t.equal(getActionByKey('create-exchange'), createExchangeRoles);
@@ -76,8 +73,9 @@ test('should return false when user doesn\'t have the right permission for an ac
 test('should return true when user doesn\'t have the right permission for an action but the validation function returns true', t => {
   const userRoleStub = sinon.stub(authorization, 'getUserRole').returns('employee');
   const actionStub = sinon.stub(authorization, 'getActionByKey').returns({ role: 'admin', validate: () => true });
+  const exchange = { id: 1, title: 'Test shift', user: { id: 1 } };
 
-  t.true(can(employee, 'delete-exchange', exchangeA));
+  t.true(can(employee, 'delete-exchange', exchange));
   t.end();
 
   userRoleStub.restore();
@@ -85,10 +83,11 @@ test('should return true when user doesn\'t have the right permission for an act
 });
 
 test('should throw an error when user is not authorized', t => {
-  const canStub = sinon.stub(authorization, 'can').returns(false);
+  const stub = sinon.stub(authorization, 'can').returns(false);
+  const teamleader = { id: 3, username: 'ruben@flex-appeal.nl', role: 'teamleader' };
 
   t.throws(() => authorization.check(teamleader, 'create-exchange'));
   t.end();
 
-  canStub.restore();
+  stub.restore();
 });
